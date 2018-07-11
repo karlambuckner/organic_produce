b23e02975790c4a83b4beb4a71b62f5fa142f58a
Karla Buckner <kmb2662@gmail.com>
2018-07-11 10:59:31 -0700

    initial commit

---
+++ src/index.js
@@ -0,0 +1,7 @@
+var heading = React.createElement('h1', {}, "Avery's Organics");
+var app = React.createElement('div', {}, heading);
+
+ReactDOM.render(
+  greeting,
+  document.getElementById('react-app-root')
+);
