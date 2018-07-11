edc277ad4d5ab89251c98132010a568ef4e8ef2c
Karla Buckner <kmb2662@gmail.com>
2018-07-11 11:44:46 -0700

    now show html content

---
+++ src/components/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Header from "./Header";
+
+function App(){
+  return (
+    <div>
+      <Header/>
+      <h3>Season</h3>
+    </div>);
+}
+
+export default App;
--- src/index.js
+++
@@ -1,7 +0,0 @@
-var heading = React.createElement('h1', {}, "Avery's Organics");
-var app = React.createElement('div', {}, heading);
-
-ReactDOM.render(
-  greeting,
-  document.getElementById('react-app-root')
-);
